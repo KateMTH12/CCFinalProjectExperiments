44e0831e0303051b3478c73ae6eea66465aa567b
KateMTH12 <KateMTH12@users.noreply.github.com>
2017-04-17 10:40:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-function setup() {
+/*function setup() {
   createCanvas(windowWidth, windowHeight);
   noCursor();
 }
@@ -14,4 +14,53 @@ function draw() {
   textSize(50);
   stroke(0);
   text("케이트",20,50);
+}
+*/
+var mX = 0;
+var mY = 0;
+var a = 2;
+var grow = false;
+var rate = 5;
+var num = 0;
+var n = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noCursor();
+
+}
+
+function draw() {
+  n+=.001;
+  background(255);
+  /*var x;
+  var y;
+  x = map(mouseX,0,width,0,255);
+  y = map(mouseY,0,height,0,255);*/
+  noFill();
+  ellipse(mouseX, mouseY, 20, 20);
+  mX = mouseX;
+  mY = mouseY;
+  ellipse(mX, mY, a, a);
+  if (grow === true) {
+    a += rate;
+    if (a >= 200) {
+      rate = -rate;
+    }
+    if (a <= 5) {
+      rate = abs(rate);
+    }
+  }
+  
+  num = noise(n)*width;
+  fill(255,0,0);
+  rect(mouseX,0,num,height);
+}
+
+function mousePressed() {
+  if (grow === false) {
+    grow = true;
+  } else {
+    grow = false;
+  }
 }
